d26dbb1a9a730a03381c6a9f0dfca5e19289c663
linhvnde <linh.nh.tyty@gmail.com>
2023-05-22 14:15:40 +0200

    created collectible items with random image

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 console.log("connected");
+
+//Player
 class Player {
   constructor() {
     this.domElm = null;
@@ -64,7 +66,56 @@ class Player {
     }
   }
 }
+
+//collectible items
+
+class Item {
+  constructor(id, imageNum) {
+    this.domElm = null;
+    this.height = 15;
+    this.width = 15;
+    this.positionX = Math.floor(Math.random() * (100 - this.width));
+    this.positionY = Math.floor(Math.random() * (75 - this.height));
+    this.createDomElm(id, imageNum);
+  }
+  createDomElm(id, imageNum) {
+    this.domElm = document.createElement("div");
+    this.domElm.id = "item" + id;
+    this.domElm.classList.add("items");
+    this.domElm.style.color = "red";
+    this.domElm.style.backgroundImage = `url(../imgs/item${imageNum}.png)`; //can think of way to change the code to be more dynamic
+    this.domElm.style.height = this.height + "vh";
+    this.domElm.style.width = this.width + "vw";
+    this.domElm.style.left = this.positionX + "vw";
+    this.domElm.style.bottom = this.positionY + "vh";
+    const board = document.getElementById("board");
+    board.appendChild(this.domElm);
+
+    console.log("new item", this.domElm.style);
+  }
+}
+
+//game start
+
 const player = new Player();
+
+//randomly set image to items
+const numItems = 5; //if there are more images for items, change the number - also can consider to change the hardcode
+let itemArr = [];
+let usedImages = []; //track used image
+
+for (let i = 0; i < numItems; i++) {
+  let imageNum = Math.floor(Math.random() * 5) + 1;
+
+  // if the generated number is already used, find another one
+  if (usedImages.includes(imageNum)) {
+    i--; //to ensure that a new item is not created for the current i value until a unique image number is found.
+    continue;
+  }
+
+  usedImages.push(imageNum); // keep track of used image to the usedImages array
+  itemArr[i] = new Item(i + 1, imageNum);
+}
 
 //attach event listener
 document.addEventListener("keydown", (event) => {
