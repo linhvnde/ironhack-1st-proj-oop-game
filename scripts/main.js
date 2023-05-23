bd00f82e2085c3ecdfc746d46c5d3ec9ca053d34
linhvnde <linh.nh.tyty@gmail.com>
2023-05-23 19:20:47 +0200

    added obs class and gameover

--- scripts/main.js
+++ scripts/main.js
@@ -78,22 +78,22 @@ class Player {
 
 //collectible items
 
-class Item {
-  constructor(id, imageNum) {
+class GameObj {
+  constructor(type, id, imageNum) {
+    this.type = type;
     this.imageNum = imageNum;
     this.domElm = null;
     this.height = 15;
     this.width = 10;
     this.positionX = Math.floor(Math.random() * (100 - this.width));
     this.positionY = Math.floor(Math.random() * (75 - this.height));
-    this.createDomElm(id, imageNum);
+    this.createDomElm(type, id, imageNum);
   }
-  createDomElm(id, imageNum) {
+  createDomElm(type, id, imageNum) {
     this.domElm = document.createElement("div");
-    this.domElm.id = "item" + id;
-    this.domElm.classList.add("items");
-    this.domElm.style.color = "red";
-    this.domElm.style.backgroundImage = `url(../imgs/item${imageNum}.png)`; //can think of way to change the code to be more dynamic
+    this.domElm.id = `${type}` + id;
+    this.domElm.classList.add(type);
+    this.domElm.style.backgroundImage = `url(../imgs/${type}${imageNum}.png)`; //dynamically change the images
     this.domElm.style.height = this.height + "vh";
     this.domElm.style.width = this.width + "vw";
     this.domElm.style.left = this.positionX + "vw";
@@ -103,7 +103,41 @@ class Item {
     // console.log("new item", this.domElm.style);
   }
 }
-
+class Item extends GameObj {
+  constructor(id, imageNum) {
+    super("item", id, imageNum);
+  }
+}
+class Obstacle extends GameObj {
+  constructor(id, imageNum) {
+    super("obs", id, imageNum);
+    this.positionY = 100 - this.height;
+    this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+    this.newPos();
+    this.move();
+  }
+  move() {
+    setInterval(() => {
+      this.newPos();
+    }, 20);
+  }
+  newPos() {
+    if (this.positionY < 0) {
+      clearInterval(this.move);
+      this.domElm.remove();
+    } else {
+      this.gravitySpeed += this.gravity;
+      this.positionX += this.speedX;
+      this.positionY -= this.speedY + this.gravitySpeed;
+      this.domElm.style.left = this.positionX + "vw";
+      this.domElm.style.bottom = this.positionY + "vh";
+    }
+  }
+}
 //game start
 
 //create items & randomly set image to items
@@ -127,14 +161,14 @@ const createItem = (numImgs, id) => {
 
   itemArr.push(new Item(id, imageNum));
 };
-
+//create items
 const updateItems = (numImgs) => {
   let id = 0;
   setInterval(() => {
     createItem(numImgs, id + 1);
     console.log("item create", id);
   }, itemCreationDelay);
-
+  //remove items
   const removeItem = () => {
     if (itemArr.length > 0) {
       itemArr[0].domElm.remove();
@@ -145,40 +179,72 @@ const updateItems = (numImgs) => {
   setTimeout(() => {
     setInterval(removeItem, itemRemovalDelay);
   }, itemLifeTime); //delay removeItem after its itemLifeTime
-
-  //check collision in very milisecond
-  setInterval(() => {
-    for (let i = 0; i < itemArr.length; i++) {
-      if (player.isColliding(itemArr[i])) {
-        if (itemArr[i].imageNum >= 1 && itemArr[i].imageNum <= 10) {
-          player.score += 1;
-          const displayElm = document.getElementById("score-display");
-          displayElm.innerText = player.score;
-          console.log(`Score: ${player.score}`);
-        } else {
-          //display warning
-          let warningDisplay = document.getElementById("warning-display");
-          if (warningDisplay.style.display === "none") {
-            warningDisplay.style.display = "block";
-            setTimeout(() => {
-              warningDisplay.style.display = "none";
-            }, 800);
-          } else {
-            warningDisplay.style.display = "none";
-          }
-        }
-        // Remove the item
-        itemArr[i].domElm.remove();
-        itemArr.splice(i, 1);
-        i--; // Decrement i so we don't skip the next item
-      }
-    }
-  }, 1);
 };
-
+// Initialization of player and item images
 const player = new Player();
 const numImgs = 21; //if there are more images for items, change the number - also can consider to change the hardcode
 updateItems(numImgs);
+
+////create obstacles & randomly set image to obstacles
+let obstacleArr = [];
+const obstacleCreationDelay = 1000;
+//create obstacles
+const createObstacle = (numImgs, id) => {
+  let imageNum = Math.floor(Math.random() * numImgs) + 1;
+  obstacleArr.push(new Obstacle(id, imageNum));
+};
+//update obstacles
+const updateObstacles = (numImgs) => {
+  let id = 0;
+  setInterval(() => {
+    createObstacle(numImgs, id + 1);
+    console.log("obstacle create", id);
+  }, obstacleCreationDelay);
+};
+
+const numObstacleImgs = 8;
+updateObstacles(numObstacleImgs);
+
+const gameOver = () => {
+  location.href = "./gameover.html";
+};
+
+//check collision in very milisecond
+setInterval(() => {
+  //item collisions actions
+  for (let i = 0; i < itemArr.length; i++) {
+    if (player.isColliding(itemArr[i])) {
+      if (itemArr[i].imageNum >= 1 && itemArr[i].imageNum <= 10) {
+        player.score += 1;
+        const displayElm = document.getElementById("score-display");
+        displayElm.innerText = player.score;
+        console.log(`Score: ${player.score}`);
+      } else {
+        //display warning
+        let warningDisplay = document.getElementById("warning-display");
+        if (warningDisplay.style.display === "none") {
+          warningDisplay.style.display = "block";
+          setTimeout(() => {
+            warningDisplay.style.display = "none";
+          }, 800);
+        } else {
+          warningDisplay.style.display = "none";
+        }
+      }
+      // Remove the item
+      itemArr[i].domElm.remove();
+      itemArr.splice(i, 1);
+      i--; // Decrement i so we don't skip the next item
+    }
+  }
+  //obstacle collisions actions
+  for (let i = 0; i < obstacleArr.length; i++) {
+    if (player.isColliding(obstacleArr[i])) {
+      gameOver();
+      return;
+    }
+  }
+}, 1);
 
 //attach event listener
 document.addEventListener("keydown", (event) => {
